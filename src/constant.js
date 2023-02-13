247b5ed4fc608586d9cea6088adce8f5c82cb3f7
Zakir Hussain <itsjakirhussain@gmail.com>
2023-02-14 00:38:17 +0530

    Hero, Skill and service

---
+++ src/constant.js
@@ -0,0 +1,145 @@
+export const roleData = [
+  "Software Engineer",
+  "Javascript expert",
+  "Front-end Developer",
+  "Back-end Developer",
+  "Node JS Developer",
+  "React JS Developer",
+];
+
+export const boardData = [
+  {
+    title: "Years of Experience",
+    value: 5,
+    duration: 1,
+  },
+  {
+    title: "Projects worked",
+    value: 20,
+    duration: 2,
+  },
+  {
+    title: "Known Technology",
+    value: 50,
+    duration: 3,
+  },
+];
+
+export const docsData = {
+  download:
+    "https://file.jhussain.com/resume/jakir_hussain_resume_compressed.pdf",
+  hire_me: "https://www.upwork.com/freelancers/~01962ee1e2e11317a4",
+};
+export const socialData = [
+  {
+    src: "/svg/github.svg",
+    height: 25,
+    width: 25,
+    alt: "GitHub",
+    href: "https://github.com/iam-hussain",
+  },
+  {
+    src: "/svg/gitlab.svg",
+    height: 25,
+    width: 25,
+    alt: "GitLab",
+    href: "https://gitlab.com/iam-hussain",
+  },
+  {
+    src: "/svg/upwork.svg",
+    height: 25,
+    width: 50,
+    alt: "UpWorks",
+    href: "https://www.upwork.com/freelancers/~01962ee1e2e11317a4",
+  },
+  {
+    src: "/svg/linkedin.svg",
+    height: 25,
+    width: 25,
+    alt: "LinkedIn",
+    href: "https://www.linkedin.com/in/iam-hussain",
+  },
+  {
+    src: "/svg/npm.svg",
+    height: 25,
+    width: 35,
+    alt: "NPM",
+    href: "https://www.npmjs.com/~iam-hussain",
+  },
+  {
+    src: "/svg/stackoverflow.svg",
+    height: 25,
+    width: 25,
+    alt: "StackOverFlow",
+    href: "https://stackoverflow.com/users/13712263/hussain",
+  },
+  {
+    src: "/svg/twitter.svg",
+    height: 25,
+    width: 25,
+    alt: "Twitter",
+    href: "https://twitter.com/_i_am_hussain",
+  },
+];
+
+export const skillsData = [
+  {
+    title: "TOP SKILLS",
+    items: [
+      "ReactJS / NextJS / Astro",
+      "NodeJS / ExpressJS",
+      "GraphQL / Rest API",
+      "Styled Component",
+      "Solidity / ERC Tokens",
+    ],
+  },
+  {
+    title: "DATABASE",
+    items: ["MySQL", "PostgreSQL", "MongoDB", "DynamoDB", "Redis"],
+  },
+  {
+    title: "LANGUAGES",
+    items: [
+      "JavaScript",
+      "TypeScript",
+      "GraphQL",
+      "HTML",
+      "CSS / SCSS / LESS",
+      "Solidity",
+    ],
+  },
+  {
+    title: "Framework / Libraries",
+    items: [
+      "Bootstrap / Tailwind",
+      "Material UI",
+      "Apollo GraphQL",
+      "Sequelize / Mongoose",
+      "Jest / Mocha / Chai",
+      "Storybook",
+      "Cypress.IO",
+      "Webpack / Babel",
+      "Elastic Search",
+      "Socket.IO",
+      "HandleBar JS / EJS",
+      "Web3 JS / Ethers JS",
+    ],
+  },
+
+  {
+    title: "Tools / Others",
+    items: [
+      "Redux",
+      "Nginx",
+      "Docker",
+      "AWS",
+      "Serverless Computing",
+      "Cloud Computing",
+      "Truffle / Hardhat",
+    ],
+  },
+  // {
+  //   title: "learning",
+  //   items: ["React Native", "Flutter / Dart"],
+  // },
+];
